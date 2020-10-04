0a0d559b6b331c6b686df3ea009d9f8797b95f30
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-10-04 15:08:53 +0300

    refactor: redone the solution

--- src/index.js
+++ src/index.js
@@ -1,41 +1,47 @@
 module.exports = function toReadable(number) {
-    let humanReadableNumber = 'None';
+    let humanReadableNumber;
 
-    const UNITS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const ONE_WORD_NUMBERS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
     const DOZENS = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    if (number < 20) {
-        humanReadableNumber = UNITS[number];
+    if (0 <= number && number < 20) {
+        humanReadableNumber = ONE_WORD_NUMBERS[number];
     }
 
     else if (number < 100) {
-        let remainder = number % 10;
-        if (remainder == 0) {
+        let remainder10 = number % 10;
+        if (remainder10 === 0) {
             humanReadableNumber = DOZENS[(number / 10)];
         }
         else {
-            let dozens = number - remainder;
-            humanReadableNumber = DOZENS[(dozens / 10)] + ' ' + UNITS[remainder];
+            let dozens = number - remainder10;
+            humanReadableNumber = DOZENS[(dozens / 10)] + ' ' + ONE_WORD_NUMBERS[remainder10];
         }
     }
     else if (number < 1000) {
-        let remainder2 = number % 100;
-        if (remainder2 == 0) {
-            humanReadableNumber = UNITS[(number / 100)] + ' hundred';
+        let remainder100 = number % 100;
+        let remainder10 = number % 10;
+        if (remainder100 === 0) {
+            humanReadableNumber = ONE_WORD_NUMBERS[(number / 100)] + ' hundred';
         }
-        else if (remainder2 % 10 == 0) {
-            humanReadableNumber = UNITS[((number - remainder2) / 100)] + ' hundred ' + DOZENS[(remainder2 / 10)];
+        else if (remainder100 % 10 === 0) {
+            humanReadableNumber = ONE_WORD_NUMBERS[((number - remainder100) / 100)] + ' hundred ' + DOZENS[(remainder100 / 10)];
         }
-        else if (10 < remainder2 && remainder2 < 20) {
-            humanReadableNumber = UNITS[((number - (number % 100)) / 100)] + ' hundred ' + UNITS[(number % 100)];
+        else if (10 < remainder100 && remainder100 < 20) {
+            humanReadableNumber = ONE_WORD_NUMBERS[((number - remainder100) / 100)] + ' hundred ' + ONE_WORD_NUMBERS[remainder100];
         }
-        else if (remainder2 < 10) {
-            humanReadableNumber = UNITS[((number - (number % 100)) / 100)] + ' hundred ' + UNITS[(number % 10)];
+        else if (remainder100 < 10) {
+            humanReadableNumber = ONE_WORD_NUMBERS[((number - remainder100) / 100)] + ' hundred ' + ONE_WORD_NUMBERS[remainder10];
+        }
+        else if (remainder100 >= 20) {
+            humanReadableNumber = ONE_WORD_NUMBERS[((number - remainder100) / 100)] + ' hundred ' + DOZENS[((remainder100 - remainder10) / 10)] + ' ' + ONE_WORD_NUMBERS[remainder10];
         }
         else {
-            humanReadableNumber = UNITS[((number - (number % 100)) / 100)] + ' hundred ' + DOZENS[(((number % 100) - (number % 10)) / 10)] + ' ' + UNITS[(number % 10)];
+            console.log('given number is out of range 100 to 999');
         }
+    } else {
+        console.log('given number is out of range 0 to 999');
     }
 
 
